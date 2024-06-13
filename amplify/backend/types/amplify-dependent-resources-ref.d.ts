2627ae5994d09c3f817c63de278feae053bc2510
Rahul Karthik <rahulkarthik@Rahuls-MacBook-Air.local>
2024-06-13 12:49:26 -0700

    Added Authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,23 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyapp1964a452": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "amplifyapp1964a452CustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ src/home.js
@@ -0,0 +1,9 @@
+function home(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <p>My First AWS Amplify Deployment</p>
+            </header>
+        </div>
+    )
+}
